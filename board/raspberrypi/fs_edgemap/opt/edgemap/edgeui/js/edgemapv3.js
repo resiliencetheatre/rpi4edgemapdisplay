34518522e0a8f63a1e2c7d22420f7c776989c35f
info <info@resilience-theatre.com>
2024-06-20 04:32:18 +0300

    Sensor icon code clean up.

--- board/raspberrypi/fs_edgemap/opt/edgemap/edgeui/js/edgemapv3.js
+++ board/raspberrypi/fs_edgemap/opt/edgemap/edgeui/js/edgemapv3.js
@@ -1374,12 +1374,10 @@ function loadCallSign() {
 
 // Work in progress
 function loadSensor(id) {
-    // console.log("#1 loadSensor(" + id + ") " + id);
     fetch('load_sensor.php?id=' + id )
     .then(response => response.json())
     .then(data => {
         const sensorDataArray = data.data.split(",");
-        // addSensorDot(sensorDataArray[2],sensorDataArray[1],sensorDataArray[0]);
         addSensorIcon(sensorDataArray[2],sensorDataArray[1],sensorDataArray[0]);
         return;
     })
@@ -1389,130 +1387,6 @@ function loadSensor(id) {
     });
 }
 
-//
-// Example from maplibre-gl pulsedot
-//
-function addSensorDot(lon,lat,sensorText) {
-    const size = 100;
-    const pulsingDot = {
-        width: size,
-        height: size,
-        data: new Uint8Array(size * size * 4),
-
-        // get rendering context for the map canvas when layer is added to the map
-        onAdd () {
-            const canvas = document.createElement('canvas');
-            canvas.width = this.width;
-            canvas.height = this.height;
-            this.context = canvas.getContext('2d');
-        },
-        // called once before every frame where the icon will be used
-        render () {
-            const duration = 1000;
-            const t = (performance.now() % duration) / duration;
-            const radius = (size / 2) * 0.3;
-            const outerRadius = (size / 2) * 0.7 * t + radius;
-            const context = this.context;
-            // draw outer circle
-            context.clearRect(0, 0, this.width, this.height);
-            context.beginPath();
-            context.arc(
-                this.width / 2,
-                this.height / 2,
-                outerRadius,
-                0,
-                Math.PI * 2
-            );
-            context.fillStyle = `rgba(255, 200, 200,${1 - t})`;
-            context.fill();
-            // draw inner circle
-            context.beginPath();
-            context.arc(
-                this.width / 2,
-                this.height / 2,
-                radius,
-                0,
-                Math.PI * 2
-            );
-            context.fillStyle = 'rgba(255, 100, 100, 1)';
-            context.strokeStyle = 'white';
-            context.lineWidth = 2 + 4 * (1 - t);
-            context.fill();
-            context.stroke();
-            // update this image's data with data from the canvas
-            this.data = context.getImageData(
-                0,
-                0,
-                this.width,
-                this.height
-            ).data;
-            // continuously repaint the map, resulting in the smooth animation of the dot
-            map.triggerRepaint();
-            // return `true` to let the map know that the image was updated
-            return true;
-        }
-    };
-    // 
-    map.addImage('pulsing-dot', pulsingDot, {pixelRatio: 1.5});
-    map.addSource('pulsingpoints', {
-        'type': 'geojson',
-        'data': {
-            'type': 'FeatureCollection',
-            'features': [
-                {
-                    'type': 'Feature',
-                    'geometry': {
-                        'type': 'Point',
-                        'coordinates': [lon,lat]
-                    }
-                }
-            ]
-        }
-    });
-    map.addLayer({
-        'id': 'pulsepointslayer',
-        'type': 'symbol',
-        'source': 'pulsingpoints',
-        'layout': {
-            'icon-image': 'pulsing-dot',
-            'text-font': ['Open Sans Regular'],
-            'text-size': 12,
-            'text-anchor': 'top',
-            'text-offset': [0,1],
-            'text-transform': "uppercase",
-            'text-letter-spacing': 0.05,
-            'text-field': sensorText
-        },
-        "paint": {
-            "text-color": "#F02",
-            "text-halo-color": "#fFF",
-            "text-halo-width": 2
-        },
-    });
-    // https://maplibre.org/maplibre-style-spec/layers/#text-anchor
-    map.flyTo({
-        center: [lon,lat],
-        zoom: 12,
-        speed: 0.6,
-        curve: 1,
-        essential: true 
-    });
-        
-}
-/*function removeDot() {
-    if (map.getImage("pulsing-dot")) {
-        map.removeImage('pulsing-dot');
-    }
-    if (map.getLayer("pulsepointslayer")) {
-        map.removeLayer('pulsepointslayer');
-    }
-    if (map.getSource("pulsingpoints")) {
-        map.removeSource('pulsingpoints');
-    }
-}*/
-
-
-// https://maplibre.org/maplibre-gl-js/docs/examples/add-image/
 // Work in progress: this supports only one sensor at the moment
 async function addSensorIcon(lon,lat,sensorText) {
     
@@ -1520,7 +1394,7 @@ async function addSensorIcon(lon,lat,sensorText) {
     
         image = await map.loadImage('img/sensor-icon.png');
         map.addImage('sensor-icon', image.data);
-        map.addSource('point', {
+        map.addSource('sensorPoint', {
             'type': 'geojson',
             'data': {
                 'type': 'FeatureCollection',
@@ -1539,7 +1413,7 @@ async function addSensorIcon(lon,lat,sensorText) {
         map.addLayer({
             'id': 'sensorIconLayer',
             'type': 'symbol',
-            'source': 'point',
+            'source': 'sensorPoint',
             'layout': {
                 'icon-image': 'sensor-icon',
                 'icon-size': 0.35,
@@ -1581,7 +1455,17 @@ async function addSensorIcon(lon,lat,sensorText) {
     }
 }
 
-
+function removeSensorIcon() {
+    if (map.getImage("sensor-icon")) {
+        map.removeImage('sensor-icon');
+    }
+    if (map.getLayer("sensorIconLayer")) {
+        map.removeLayer('sensorIconLayer');
+    }
+    if (map.getSource("sensorPoint")) {
+        map.removeSource('sensorPoint');
+    }
+}
 
 
 
